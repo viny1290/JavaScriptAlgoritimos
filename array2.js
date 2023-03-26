9d54203cd0fdce4431094315b711593ea98b8bea
viny1290 <vinijudo532@gmail.com>
2023-03-25 22:28:51 -0300

    JavaScript algoritimos

---
+++ array2.js
@@ -0,0 +1,36 @@
+const listaLivros = [
+    {
+        titulo: "C++",
+        preco: 35
+    },
+    {
+        titulo: "Scala",
+        preco: 40
+    },
+    {
+      titulo: "JavaScript",
+      preco: 25
+    },
+    {
+      titulo: "Java",
+      preco: 30
+    },
+    {
+        titulo: "Rust",
+        preco: 22
+      },
+    {
+      titulo: "Go",
+      preco: 45
+    },
+    {
+      titulo: "Elixir",
+      preco: 50
+    },
+    {
+        titulo: "PHP",
+        preco: 15
+    }
+]
+
+module.exports = listaLivros;
